4a15929daa1c96c53f4ecbcd65062275b2b241d5
jerryespi13 <jerryespi@gmail.com>
2023-08-22 18:58:03 -0600

    validacion de datos en el form de register

--- application.py
+++ application.py
@@ -1,8 +1,8 @@
 import os
 
-from flask import Flask, session, render_template, request, redirect
+from flask import Flask, session, render_template, request, flash
 from flask_session import Session
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, bindparam, String
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
@@ -31,15 +31,54 @@ def register():
         return render_template("register.html")
     
     else:
-        # TO DO
-        #query = text("SELECT * FROM usuarios")
-        #datos = db.execute(query).fetchall()
-        #print(datos)
+        # obtenemos los datos del formulario
         nombre = request.form.get("name")
         usuario = request.form.get("username")
         contraseña = request.form.get("password")
         confirmacion_contraseña = request.form.get("confirmation")
-        print(nombre + " " + usuario + " " + contraseña + " "+ confirmacion_contraseña)
+
+        # nos aseguramos que todos los datos se hayan ingresedo
+        # en los campos del formulario
+        if not nombre:
+            flash("Introducir campo nombre")
+            return render_template("register.html")
+        elif not usuario:
+            flash("Ingrese campo usuario")
+            return render_template("register.html")
+        elif not contraseña:
+            flash("Ingrese campo contraseña")
+            return render_template("register.html")
+        elif not confirmacion_contraseña:
+            flash("Ingrese campo confirmacion contraseña")
+            return render_template("register.html")
+        elif contraseña != confirmacion_contraseña:
+            flash("Contraseñas deben de ser iguales")
+            return render_template("register.html")
+
+        # confirmamos que el usuario no exista
+        # hacemos la consulta SQL
+        query = text(
+                        """
+                            SELECT * FROM usuarios WHERE usuario = :usuario
+                        """
+                    )
+        # pasamos los parametros
+        query = query.bindparams(
+                                    bindparam("usuario", type_=String())
+                                )
+        # ejecutamos la consulta
+        resultado = db.execute(query, {"usuario":usuario}).fetchall()
+        
+        # comparamos el resultado de la columna usuario en la db 
+        # devuelto por la consulta con el usuario ingresado por el usuario
+        
+        if len(resultado) != 0:
+            print(len(resultado))
+            # si el usuario existe lo notificamos para que elija otro nombre de usuario
+            if resultado[0].usuario == usuario:
+                flash('usuario ya existe')
+                return render_template("register.html")
+        
         return "Hacer Metodo POST"
 
 @app.route("/login", methods=["GET","POST"])
