343aef6d7a03e981e5f518fe42e86d685e649a08
jerryespi13 <jerryespi@gmail.com>
2023-09-05 09:55:19 -0600

    ajuste del css para la vista detallelibro

--- static/js/createRating.js
+++ static/js/createRating.js
@@ -1,8 +1,8 @@
 function createRating(){
     let rating = document.getElementById("average").value
     total = 5
-    startIcon = '<img src="/static/images/star-fill.svg" alt="Estrella llena" style="width: 3rem">'
-    emptIcon = '<img src="/static/images/star.svg" alt="Estrella vacia" style="width: 3rem">'
+    startIcon = '<img src="/static/images/star-fill.svg" alt="Estrella llena">'
+    emptIcon = '<img src="/static/images/star.svg" alt="Estrella vacia">'
     const start = startIcon.repeat(rating)
     const empty = emptIcon.repeat(Math.ceil(total - rating))
     document.getElementById("estrellas").innerHTML= start + empty
