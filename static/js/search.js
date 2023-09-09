a3714068fda1deb1ab966c0ab9748abd657e384e
jerryespi13 <jerryespi@gmail.com>
2023-09-09 01:36:42 -0600

    scroll hacia input de busqueda al mostrar resultados de busqueda

---
+++ static/js/search.js
@@ -0,0 +1,4 @@
+// desplazar la pagina para que quede arriba la pagina de busqueda
+let InputArriba = document.querySelector(".input-search");
+// Desplazar la página hasta el input
+InputArriba.scrollIntoView();
